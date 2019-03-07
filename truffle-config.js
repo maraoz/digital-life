42168182f1aa267c1e9c07bbbdf590e00b73dc66
Manuel Araoz <manuelaraoz@gmail.com>
2019-03-07 16:46:34 -0300

    config files update

--- truffle-config.js
+++ truffle-config.js
@@ -8,6 +8,13 @@ module.exports = {
       gas: 5000000,
       gasPrice: 5e9,
       network_id: '*'
+    },
+    ropsten: {
+      host: 'localhost',
+      port: 18545,
+      gas: 6000000,
+      gasPrice: 15e9,
+      network_id: '3'
     }
   }
 };
